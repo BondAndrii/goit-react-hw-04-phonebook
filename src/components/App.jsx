6bdc5c52450ea056405148d14015f3a214af7c30
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2022-12-25 16:34:31 +0200

    deleted all comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,9 @@ import contactsList from "../../src/data/contacts.json"
 import "./App.css";
 
 export default function App() {
-//   const initialValue =[
-//       { "id": "id-1", "name": "Rosie Simpson", "number": "459-12-56" },
-//       { "id": "id-2", "name": "Hermione Kline", "number": "443-89-12" },
-//       { "id": "id-3", "name": "Eden Clements", "number": "645-17-79" },
-//       { "id": "id-4", "name": "Annie Copeland", "number": "227-91-26" }
-// ]
+
   const [contacts, setContacts] = useState(() => (JSON.parse(window.localStorage.getItem('contactsArr')) ?? contactsList)); 
-  // didMount, як все запрацює, попробувать ліниву ініціалізацію
+ 
   const [filter, setFilter] = useState('');
 
   const formSubmitHandler = data => {
@@ -27,20 +22,18 @@ export default function App() {
           
           setContacts(contacts => [data, ...contacts]);
           console.log("in app", contacts, filter);
-      // this.setState(({ contacts }) => ({
-      //   contacts: [...contacts, data],
-      //   }));
+      
     }  
   }
   useEffect(()=>{
     window.localStorage.setItem('contactsArr', JSON.stringify(contacts))
   }, [contacts]);
   
-  const doFilter = (e) => {//поки так, але можливо тут ще useEffect потрібен 
+  const doFilter = (e) => { 
     setFilter(e.currentTarget.value)
   }
   const doClear = () => {
-    setFilter(''); // як запрацює основний код, замість doClear можна спробувати передати просто SetFilter
+    setFilter(''); 
   }
  function toFoundAbonent(a,b) {
     const normalizedFilter = a.toLowerCase();
@@ -50,13 +43,7 @@ export default function App() {
   };
  const arr = toFoundAbonent(filter, contacts)
 
-  // const  toFoundAbonent = () => {
-  //     const normalizedFilter = filter.toLowerCase();
-  //     return contacts.filter(abon =>
-  //    // console.log("abon", abon)
-  //     abon.name.toLowerCase().includes(normalizedFilter)
-  //    );  
-  // }
+  
   const deleteContact = (contactId) => { 
     setContacts((contacts) => (contacts.filter(contact => contact.id !== contactId)));
   };
@@ -72,76 +59,3 @@ export default function App() {
     </div>
   );
 }
-
-
-//----------------------------------CLASS----------------------------------//
-// class App extends Component {
-//   state = {
-//    contacts: [
-//     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-//     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-//     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-//     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-//   ],
-//   filter:'',
-//   }  
-//   formSubmitHandler = data => {
-//         if (this.state.contacts.find(contact => contact.name === data.name)) {
-//       const message = `Абонент ${data.name} вже є в книзі`;
-//       alert(message);  
-//     }
-//         else {
-          
-//       this.setState(({ contacts }) => ({
-//         contacts: [...contacts, data],
-//       }));
-//           console.log("in app", this.state)
-//     }  
-//   }
-//   componentDidMount() {     
-//     const myContactsFromLocalStorage = JSON.parse(localStorage.getItem('contactsArr'));    
-//     if (myContactsFromLocalStorage) {
-//       this.setState({ contacts: myContactsFromLocalStorage });
-//     }    
-//   }
-//   componentDidUpdate(prevProps, prevState) {    
-//     if (this.state.contacts!== prevState.contacts) {     
-//       localStorage.setItem('contactsArr', JSON.stringify(this.state.contacts));     
-//     }
-//   }  
-//   doFilter = (e) => {
-//     this.setState({ filter: e.currentTarget.value });       
-//   }
-//   doClear = () => {
-//     this.setState({ filter: '' })
-//   }
-//   toFoundAbonent = () => {
-//     const { contacts, filter } = this.state;
-//     const normalizedFilter = filter.toLocaleLowerCase();
-//     return contacts.filter(abon =>
-//       // console.log("abon", abon)
-//       abon.name.toLocaleLowerCase().includes(normalizedFilter)
-//     );
-//   }
-//   deleteContact = (contactId) => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-//   render() {
-//     const { filter} = this.state;
-//     const foundAbonent = this.toFoundAbonent();
-//     return (
-//     <div className="Container">
-//       <h1 className="Tittle">Записник контактів</h1>
-//       <Form priSubmit={this.formSubmitHandler} />      
-//       <div>
-//         <h2 className="SecondTittle">Контакти</h2>
-//         <Filter value={filter} onChange={this.doFilter} onDelete={this.doClear } />
-//         <ContactList contacts={foundAbonent} onDelete={this.deleteContact} />
-//       </div>
-//     </div>
-//   );
-// };
-// };
-// export default App;
--- src/components/Phonebook/Form/Form.js
+++ src/components/Phonebook/Form/Form.js
@@ -1,5 +1,4 @@
 import React, { useState} from "react";
-// import { Component } from "react";
 import { nanoid } from "nanoid";
 import PropTypes from "prop-types";
 import "./Form.css"
@@ -30,25 +29,12 @@ export default function Form({priSubmit}) {
                 return;
         }
         
-        // const field = (name) => ({
-        //     name: setName(value),
-        //     number: setNumber(value),
-            
-        // }[name])
-        // this.setState({
-        //     id: nanoid(),  
-        //     [name]: value,
-                      
-        // }
-        // );      
+          
     }
     const handleSubmit = e => {
         e.preventDefault();
-        const data = { id, name, number };        
-        // priSubmit(data);
-        // console.log(data);
-        reset(); 
-        console.log("in form",data);
+        const data = { id, name, number };       
+        reset();         
         return priSubmit(data);
         
     }
@@ -92,78 +78,6 @@ return (
     
 }
 
-//----------------------------------CLASS----------------------------------//
-// class Form extends Component {
-//     state = {
-//         id:'',
-//         name: '',
-//         number:'',        
-//     }
-//     nameId = nanoid();
-//     numberId = nanoid();
-//     handleChange = e => {
-//         const { name, value } = e.currentTarget;
-//         this.setState({
-//             id: nanoid(),  
-//             [name]: value,
-                      
-//         });      
-//     }
-//     handleSubmit = e => {
-//     e.preventDefault();
-        
-//         this.props.priSubmit(this.state);        
-//         console.log("in form after", this.state);
-//         this.reset();      
-//     }
-//     reset = () => {
-//         this.setState({ id:'', name: '', number:'', });
-//     };
-//     render() {
-//         return (
-            
-//             <form className="Form" onSubmit={this.handleSubmit}>
-//                 <label className="Label" htmlFor={this.nameId}>
-//                     <p className="Text">Iм'я:</p>
-//                     <input
-//                         type="text"
-//                         name="name"
-//                         className="Input"
-//                         id={this.nameId}
-//                         value={this.state.name}
-//                         onChange={this.handleChange}
-//                         placeholder="введи ім'я"
-//                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//                         required
-//                     />
-//                 </label>
-               
-//                 <label className="Label" htmlFor={this.numberId}>
-//                     <p className="Text">Номер:</p>
-//                     <input
-//                         type="tel"
-//                         name="number"
-//                         className="Input"
-//                         value={this.state.number}
-//                         id={this.numberId}
-//                         onChange={this.handleChange}
-//                         placeholder="введи номер телефону"
-//                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//                         required
-//                     />
-//                 </label>
-                
-//                 <button className="Button" type="submit">Запиши!</button>
-//             </form>
-            
-                
-//         );
-//     }
-// }
-// export default Form;
-
-Form.propTypes= {
+Form.propTypes = {
     priSubmit: PropTypes.func.isRequired,
 }
